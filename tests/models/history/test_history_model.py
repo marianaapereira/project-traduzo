6a7bc704290ff41633421d6931dc18750d751d3e
Mariana Pereira <marianapereira.s@live.com>
2024-04-29 13:53:55 -0300

    Correções de lint

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -6,17 +6,23 @@ from src.models.history_model import HistoryModel
 def test_request_history():
     history = HistoryModel.list_as_json()
     history_json = json.loads(history)
-    
+
     history_1 = {
         "text_to_translate": "Hello, I like videogame",
         "translate_from": "en",
         "translate_to": "pt",
     }
-    
+
     history_2 = {
         "text_to_translate": "Do you love music?",
         "translate_from": "en",
         "translate_to": "pt",
     }
 
-    assert history_1["text_to_translate"] == history_json[0]['text_to_translate']
+    assert (
+        history_1["text_to_translate"] == history_json[0]["text_to_translate"]
+    )
+
+    assert (
+        history_2["text_to_translate"] == history_json[1]["text_to_translate"]
+    )
