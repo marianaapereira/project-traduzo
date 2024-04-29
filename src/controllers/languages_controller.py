0bffc3fc3cd6059f16ff5f9c11350e286d91af72
Mariana Pereira <marianapereira.s@live.com>
2024-04-29 13:36:39 -0300

    Implementação da tradução e tradução reversa de texto

--- src/controllers/languages_controller.py
+++ src/controllers/languages_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 languages_controller = Blueprint("languages_controller", __name__)
 
@@ -11,7 +12,6 @@ def index():
     translate_from = "pt"
     translate_to = "en"
     translated = "What do you want to translate?"
-    print(languages)
 
     return render_template(
         "index.html",
@@ -20,4 +20,46 @@ def index():
         translate_from=translate_from,
         translate_to=translate_to,
         translated=translated,
+    )
+
+
+@languages_controller.route("/", methods=["POST"])
+def translate():
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+    translated = GoogleTranslator(
+        source=translate_from,
+        target=translate_to
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated,
+    )
+
+
+@languages_controller.route("/reverse", methods=["POST"])
+def reverse():
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+    translated = GoogleTranslator(
+        source=translate_from,
+        target=translate_to
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=translated,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
     )
