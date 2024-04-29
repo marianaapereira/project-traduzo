960d7f7e3ef5877113ef4345ed7dc4133cbd9e6b
Mariana Pereira <marianapereira.s@live.com>
2024-04-29 11:47:51 -0300

    Criação do languages_controller

---
+++ src/controllers/languages_controller.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template
+from models.language_model import LanguageModel
+
+languages_controller = Blueprint("languages_controller", __name__)
+
+
+@languages_controller.route("/", methods=["GET"])
+def index():
+    languages = LanguageModel.list_dicts()
+    text_to_translate = "O que deseja traduzir?"
+    translate_from = "pt"
+    translate_to = "en"
+    translated = "What do you want to translate?"
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated,
+    )
